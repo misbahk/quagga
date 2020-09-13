dcc6a874105597502f3019cfac429f509e97b795
SnehaKArkachari <cngsnehaka@gmail.com>
2020-09-13 15:15:39 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,35 +1,13 @@
 import React, { Component } from 'react';
 import Scanner from './components/Scanner';
-import Result from './components/Result';
+
 
 export default class App extends Component {
-constructor(props) {
-super(props);
-this.state = {
-scanning: false,
-results: [],
-};
-this._scan = this._scan.bind(this);
-this._onDetected = this._onDetected.bind(this);
-}
 
-_scan() {
-this.setState({ scanning: !this.state.scanning });
-}
-
-_onDetected(result) {
-this.setState({ results: this.state.results.concat([result]) });
-}
 render() {
 return (
 <div>
-<button onClick={this._scan}>{this.state.scanning ? 'Stop' : 'Start'}</button>
-<ul className="results">
-{this.state.results.map(result => {
-
-})}
-</ul>
-{this.state.scanning ? <Scanner onDetected={this.state._onDetected} /> : null}
+<Scanner/>
 </div>
 );
 }
--- src/components/Scanner.js
+++ src/components/Scanner.js
@@ -1,44 +1,221 @@
 import React, { Component } from 'react';
 import Quagga from 'quagga';
-import Brcode from '../brcode.png'
 
+import ReactCrop from 'react-image-crop';
+import 'react-image-crop/dist/ReactCrop.css';
+import { Card } from 'react-bootstrap';
+var result;
 export default class Scanner extends Component {
-constructor(props) {
-super(props);
-this._onDetected = this._onDetected.bind(this);
+state = {
+src: null,
+crop: '',
+field:false,
+imagearea:false
 }
 
-componentDidMount() {
-    console.log("working")
-    Quagga.decodeSingle({
-        src: Brcode,
-        numOfWorkers: 0, // Needs to be 0 when used within node
-        inputStream: {
-        size: 400 // restrict input-size to be 800px in width (long-side)
-        },
-        decoder: {
-        readers: ["ean_reader"] // List of active readers
-        },
-        }, function(result) {
-            console.log("resulttt", result)
-        if(result.codeResult) {
-        console.log("result", result.codeResult.code);
-        } else {
-        console.log("not detected");
-        }
-        });
-Quagga.onDetected(this._onDetected);
+
+
+componentDidMount(){
+localStorage. clear();
 }
 
-componentWillUnmount() {
-Quagga.offDetected(this._onDetected);
+onSelectFile = e => {
+if (e.target.files && e.target.files.length > 0) {
+const reader = new FileReader();
+reader.addEventListener('load', () =>
+this.setState({ src: reader.result })
+);
+reader.readAsDataURL(e.target.files[0]);
+}
+this.setState({
+imagearea:true
+})
+};
+onImageLoaded = image => {
+this.imageRef = image;
+};
+
+onCropComplete = crop => {
+this.makeClientCrop(crop);
+};
+
+onCropChange = (crop, percentCrop) => {
+// You could also use percentCrop:
+// this.setState({ crop: percentCrop });
+this.setState({ crop });
+};
+
+async makeClientCrop(crop) {
+if (this.imageRef && crop.width && crop.height) {
+const croppedImageUrl = await this.getCroppedImg(
+this.imageRef,
+crop,
+'newFile.jpeg'
+);
+this.setState({ croppedImageUrl });
 }
 
-_onDetected(result) {
-this.props.onDetected(result);
 }
+someFunction = ()=>{
+let local = localStorage.getItem("CODE")
+result = local
+this.setState({
+field:true
+})
+}
+getCroppedImg(image, crop, fileName) {
+Quagga.decodeSingle({
+src: this.state.src,
+numOfWorkers: 1,
+inputStream: {
+size: 800
+},
+decoder: {
+readers: ["code_128_reader", "code_39_reader", "code_39_vin_reader", "ean_reader", "ean_8_reader", "upc_reader", "upc_e_reader", "codabar_reader"]
+},
+}, function(result) {
+if(result.codeResult) {
+
+setTimeout(() => {
+localStorage.setItem("CODE",result.codeResult.code)
+},3000);
+} else {
+localStorage.setItem("CODE","NOT DETECTED")
+}
+});
+setTimeout(() => {
+this.someFunction()
+},5000);
+const canvas = document.createElement('canvas');
+const scaleX = image.naturalWidth / image.width;
+const scaleY = image.naturalHeight / image.height;
+canvas.width = crop.width;
+canvas.height = crop.height;
+const ctx = canvas.getContext('2d');
+
+ctx.drawImage(
+image,
+crop.x * scaleX,
+crop.y * scaleY,
+crop.width * scaleX,
+crop.height * scaleY,
+0,
+0,
+crop.width,
+crop.height
+);
+return new Promise((resolve, reject) => {
+canvas.toBlob(blob => {
+if (!blob) {
+//reject(new Error('Canvas is empty'));
+console.error('Canvas is empty');
+return;
+}
+blob.name = fileName;
+window.URL.revokeObjectURL(this.fileUrl);
+this.fileUrl = window.URL.createObjectURL(blob);
+resolve(this.fileUrl);
+}, 'image/jpeg');
+});
+}
+
+
+
+
+
+
+
 
 render() {
-return <div id="interactive" className="viewport" />;
+const { crop, croppedImageUrl, src } = this.state;
+return (
+<>
+
+
+<div style={{position:"absolute", top:"50%", left:"50%", transform:"translateY(-50%)"}}>
+
+  <div class="row m-0">
+  <Card style={{border:"2px solid grey"}}>
+  <Card.Body>
+
+  <div class="col-md-6" >
+    <center>
+  
+    
+    <input type="file" accept="image/*" onChange={this.onSelectFile} />
+
+
+    <br/>
+  
+    </center>
+    {this.state.imagearea?
+    <p style={{color:"#d249bb", fontSize:"16px", fontWeight:"bold"}}>Next..
+        <br/>
+        PLEASE SELECT THE AREA OF BARCODE IMAGE AFTER IMAGE UPLOAD</p>
+    :
+    null} 
+        </div>
+  </Card.Body>
+</Card>
+
+     
+
+    </div>
+<br/>
+
+
+    <div class="row m-0">           
+<Card style={{width:"100%", border:"2px solid grey"}}>
+  <Card.Body>
+
+<p style={{fontSize:"18px", color:"black", fontWeight:"600"}}>DETAILS:</p>
+
+{this.state.field?
+<>
+<form>
+<lable style={{color:"darkblue", fontSize:"16px", fontWeight:"600"}}>Detected Barcode {"  "}</lable>
+<input style={{color:"darkgrey", fontSize:"16px", fontWeight:"600"}} 
+type="text" value={result} disabled />
+</form>
+
+
+
+
+</>
+:
+null
+}
+
+
+ </Card.Body>
+</Card>
+</div>
+
+    
+    </div>
+  
+
+{src && (
+<ReactCrop
+src={src}
+crop={crop}
+ruleOfThirds
+onImageLoaded={this.onImageLoaded}
+onComplete={this.onCropComplete}
+onChange={this.onCropChange}
+/>
+)}
+
+
+<br/>
+{croppedImageUrl && (
+<img alt="Crop" style={{ maxWidth: '100%' }} src={croppedImageUrl} />
+)}
+
+
+
+
+</>
+)
 }
 }
